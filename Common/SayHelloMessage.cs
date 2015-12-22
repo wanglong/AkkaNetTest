f41665a128b3e58e183e6524ba1fa8e72f08f3f1
HackerVirus <nmgimust_wl@163.com>
2015-12-22 15:42:24 +0800

    akka.net demo test

---
+++ Common/SayHelloMessage.cs
@@ -0,0 +1,24 @@
+﻿using Akka.Actor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    /// <summary>
+    /// Class SayHelloMessage.
+    /// </summary>
+    public class SayHelloMessage
+    {
+    }
+
+    public class GreetingActor : ReceiveActor
+    {
+        public GreetingActor()
+        {
+            Receive<SayHelloMessage>(greet => Console.WriteLine("Hello World"));
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,41 @@
+﻿using Akka.Actor;
+using Akka.Configuration;
+using Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = ConfigurationFactory.ParseString(@"
+                        akka {  
+                            actor {
+                                provider = ""Akka.Remote.RemoteActorRefProvider, Akka.Remote""
+                            }
+                            remote {
+                                helios.tcp {
+                                    transport-class = ""Akka.Remote.Transport.Helios.HeliosTcpTransport, Akka.Remote""
+                                    applied-adapters = []
+                                    transport-protocol = tcp
+                                    port = 8081
+                                    hostname = localhost
+                                }
+                            }
+                        }
+                        ");
+            using (var system = ActorSystem.Create("MyServer", config))
+            {
+                system.ActorOf<GreetingActor>("Greeting");
+
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
